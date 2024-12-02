fcc786fcbc0c340ec58dca74d7dfbf2b2a276c8c
ahmadmbalan <ahmad.balan97@gmail.com>
2024-12-02 13:23:45 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando instância do Nokia
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "111111111111111", memoria: 64);
+        Console.WriteLine("Usando Nokia:");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Criando instância do iPhone
+        Smartphone iphone = new Iphone(numero: "654321", modelo: "iPhone 14", imei: "222222222222222", memoria: 128);
+        Console.WriteLine("Usando iPhone:");
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
